7a435093abf4686e2663ceae81f1d3a0567c47ec
Call3z <electrodez@gmail.com>
2017-09-05 16:16:50 +0200

    Added unittest for ordercontroller Edit
    Added unittest for ordercontroller Edit

--- PizzeriaUnitTests/OrderControllerUnitTests.cs
+++ PizzeriaUnitTests/OrderControllerUnitTests.cs
@@ -10,6 +10,7 @@ using Pizzeria.Controllers;
 using Pizzeria.Data;
 using Pizzeria.Models;
 using Pizzeria.Services;
+using Pizzeria.ViewModels;
 
 namespace PizzeriaUnitTests
 {
@@ -32,11 +33,34 @@ namespace PizzeriaUnitTests
 
             var controller = new OrderController(dbContext, cartMock.Object);
 
+
             //Act
             var actual = controller.Index();
 
             //Assert
             Assert.IsInstanceOfType(actual, typeof(ActionResult));
+        }
+
+        [TestMethod]
+        public void Edit_Returns_CartDish_Model()
+        {
+            //Arrange
+            var dbOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+
+            var dbContext = new ApplicationDbContext(dbOptions);
+
+            var guid = Guid.NewGuid();
+
+            var mock = new Mock<ICartService>();
+            mock.Setup(x => x.GetDish(guid)).Returns(new CartDish(){Name = "Calzone", Id = guid, ExtraIngredients = new List<IngredientViewModel>()});
+
+            var controller = new OrderController(dbContext, mock.Object);
+
+            var actual = (CartDish)((ViewResult)controller.Edit(guid)).Model;
+
+            Assert.IsTrue(actual.Name.Equals("Calzone"));
         }
     }
 }
